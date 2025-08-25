78ce69c1ad7b1e4d2e5f6dc15a81450742276845
Usman Khan <mdusmanhanifkhan@gmail.com>
2025-08-25 12:41:05 +0500

    add port in dot-env file

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+import 'dotenv/config'
+
 import express from "express"
 import routes from "./routes/index.js"
 import cors from "cors"
 const app = express()
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(cors())
 app.use(express.json())
