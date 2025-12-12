1bfda1983f46084c8816f6590f1b6adc59d89695
Usman Khan <mdusmanhanifkhan@gmail.com>
2025-12-12 20:16:02 +0500

    modify the cors

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ const app = express()
 
 const port = process.env.PORT;
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "https://hikarimed.online",
+    "https://www.hikarimed.online",
+    "http://localhost:5173",
+    "https://hms-frontend-alpha.vercel.app"  
+  ],
+  credentials: true, 
+}))
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(routes)
