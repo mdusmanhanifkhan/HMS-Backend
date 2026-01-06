881d949edf45b54a0580e99ca65341f6ff0bf57e
Usman Khan <mdusmanhanifkhan@gmail.com>
2026-01-07 03:59:45 +0500

    modify the server.js

--- server.js
+++ server.js
@@ -5,31 +5,15 @@ import routes from "./routes/index.js"
 import cors from "cors"
 const app = express()
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const FRONTEND_SERVICE_ORIGIN = process.env.CORE_ORIGIN_FRONTEND; 
 const APP_BASE_URL = process.env.APP_BASE_URL;
 const APP_BASE_URL_WWW = process.env.APP_BASE_URL_WWW;
 
-const allowedOrigins = [
-  "https://hikarimed.online",
-  "https://www.hikarimed.online",
-  "https://hikarimed.vercel.app",
-  "http://localhost:5173",
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like curl or server-to-server)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS policy: This origin is not allowed"));
-    }
-  },
-  credentials: true,
-}));
+  origin: true,
+  credentials: true, 
+}))
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
